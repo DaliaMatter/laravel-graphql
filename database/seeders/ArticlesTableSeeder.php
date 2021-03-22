c897e69fdb924963e359589ab07266154a6adacb
DaliaMatter <daliaamohamed243@gmail.com>
2021-03-22 05:17:00 +0200

    start learning graphQl with laravel.

---
+++ database/seeders/ArticlesTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Article::truncate();
+        // \App\Article::unguard();
+
+        $faker = \Faker\Factory::create();
+
+        \App\Models\User::all()->each(function ($user) use ($faker) {
+            foreach (range(1, 5) as $i) {
+                \App\Models\Article::create([
+                    'user_id' => $user->id,
+                    'title'   => $faker->sentence,
+                    'content' => $faker->paragraphs(3, true),
+                ]);
+            }
+        });
+    }
+}
